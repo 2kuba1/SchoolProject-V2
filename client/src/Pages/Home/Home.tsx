9523cdf01576170706c8b4777bce1bf60a4a5eb4
2kuba1 <jakub.wojtyna06@gmail.com>
2023-02-23 22:19:03 +0100

    Modify navbar, add image to home page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,5 +1,14 @@
+import styles from './Home.module.css';
+import ExpandMore from '../../assets/Expand_more.svg';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.HomeBackground}>
+        <img src={ExpandMore} alt='Expand more' />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
