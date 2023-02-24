7b07e7ec9553e3d3c5f40c122c36e20fff00bc13
2kuba1 <jakub.wojtyna06@gmail.com>
2023-02-24 18:59:47 +0100

    Add profiles on home page

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,6 +1,10 @@
 import styles from './Home.module.css';
 import ExpandMore from '../../assets/Expand_more.svg';
 import Class from '../../assets/Class.png';
+import MilitaryClassImg from '../../assets/MilitaryProfile.png';
+import ComputerScienceImg from '../../assets/ComuterScience.png';
+import GraphicDesignImg from '../../assets/GraphicDesign.png';
+import LogisticImg from '../../assets/LogisticProfile.png';
 
 const Home = () => {
   return (
@@ -28,10 +32,77 @@ const Home = () => {
         <div className={styles.Profiles}>
           <h1>Profiles</h1>
           <div className={styles.ProfilesContainer}>
-            <div className={styles.Military}></div>
-            <div className={styles.ComputerScience}></div>
-            <div className={styles.GrapicDesign}></div>
-            <div className={styles.Logistic}></div>
+            <div className={`${styles.Military} ${styles.Resize}`}>
+              <img
+                className={styles.ClassImage}
+                src={MilitaryClassImg}
+                alt='Military Class'
+              />
+              <div>
+                <h2 className={styles.ClassHeader}>Military Class</h2>
+                <p className={styles.ClassText}>
+                  A military profile is a record of a service member's military
+                  service, achievements, and assignments. It is used for career
+                  tracking, promotions, and retirement, and also serves as a
+                  resource for veterans to access their military records and
+                  benefits.
+                </p>
+              </div>
+            </div>
+            <div className={`${styles.ComputerScience} ${styles.Resize}`}>
+              <img
+                className={styles.ClassImage}
+                src={ComputerScienceImg}
+                alt='Computer Science Class'
+              />
+              <div>
+                <h2 className={styles.ClassHeader}>Computer Science Class</h2>
+                <p className={styles.ClassText}>
+                  A high school computer science profile teaches students the
+                  basics of programming languages like Python and Java, and
+                  focuses on building skills in software development and
+                  computer systems. It aims to prepare students for further
+                  study in computer science or related fields, or to provide
+                  them with valuable skills for their future careers.
+                </p>
+              </div>
+            </div>
+            <div className={`${styles.ComputerScience} ${styles.Resize}`}>
+              <img
+                className={styles.ClassImage}
+                src={GraphicDesignImg}
+                alt='Graphic Design Class'
+              />
+              <div>
+                <h2 className={styles.ClassHeader}>Graphic Design</h2>
+                <p className={styles.ClassText}>
+                  Graphic design profiles in high school teach students the
+                  skills needed to create designs for print and digital media
+                  using digital tools and software like Adobe Photoshop,
+                  Illustrator, and InDesign. The goal is to prepare students for
+                  further study in graphic design or related fields or to
+                  provide them with valuable skills for their future careers.
+                </p>
+              </div>
+            </div>
+            <div className={`${styles.Logistic} ${styles.Resize}`}>
+              <img
+                className={styles.ClassImage}
+                src={LogisticImg}
+                alt='Logistic Class'
+              />
+              <div>
+                <h2 className={styles.ClassHeader}>Logistic Class</h2>
+                <p className={styles.ClassText}>
+                  A logistics profile in high school teaches students about
+                  managing the movement of goods and services, optimizing
+                  transportation and logistics networks, and inventory control.
+                  The profile prepares students for further study in logistics
+                  or related fields or provides them with valuable skills for
+                  their future careers.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
