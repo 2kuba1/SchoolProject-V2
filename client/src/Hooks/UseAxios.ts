bfe81deae242b4c9bddd47a887647a7e1d9869b0
2kuba1 <jakub.wojtyna06@gmail.com>
2023-02-24 23:15:21 +0100

    Add useAxios hook

---
+++ client/src/Hooks/UseAxios.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+
+function useAxios<T>(
+  config: AxiosRequestConfig<any>,
+  loadOnStart: boolean = true
+): [T | undefined, boolean, string, () => void] {
+  const [data, setData] = useState<T>();
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (loadOnStart) sendRequest();
+  }, []);
+
+  const request = () => {
+    sendRequest();
+  };
+
+  const sendRequest = () => {
+    setIsPending(true);
+    axios(config)
+      .then(response => {
+        setError('');
+        setData(response.data);
+      })
+      .catch(error => setError(error.message))
+      .finally(() => setIsPending(false));
+  };
+  return [data, isPending, error, request];
+}
+
+export default useAxios;
