bf9caf6e7a9668cfffc8708ed45562689fa7c6d2
2kuba1 <jakub.wojtyna06@gmail.com>
2023-02-28 20:29:18 +0100

    Small  change in article page style

---
+++ client/src/Components/AccountMenu/AccountMenu.tsx
@@ -0,0 +1,11 @@
+import styles from './AccountMenu.module.css';
+
+const AccountMenu = () => {
+  return (
+    <div className={styles.XD}>
+      <p>jebany czarnuch</p>
+    </div>
+  );
+};
+
+export default AccountMenu;
