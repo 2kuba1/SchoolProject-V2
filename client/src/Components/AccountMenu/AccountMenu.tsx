3dad6fd72a290a9c04be0bbfc2cd693ca7c34523
2kuba1 <jakub.wojtyna06@gmail.com>
2023-03-02 20:45:23 +0100

    Add and style account menu

--- client/src/Components/AccountMenu/AccountMenu.tsx
+++ client/src/Components/AccountMenu/AccountMenu.tsx
@@ -1,9 +1,28 @@
 import styles from './AccountMenu.module.css';
+import AccountImage from '../../assets/Account_circle.svg';
+import { Link } from 'react-router-dom';
 
 const AccountMenu = () => {
   return (
-    <div className={styles.XD}>
-      <p>jebany czarnuch</p>
+    <div className={styles.Menu}>
+      <div className={styles.Links}>
+        <Link to='register'>
+          <img
+            className={styles.AccountImg}
+            src={AccountImage}
+            alt='Account icon'
+          />
+          <p>Register</p>
+        </Link>
+        <Link to='login'>
+          <img
+            className={styles.AccountImg}
+            src={AccountImage}
+            alt='Account icon'
+          />
+          <p>Login</p>
+        </Link>
+      </div>
     </div>
   );
 };
