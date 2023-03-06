c56a39af97bf3debbd26dea2e01ca482cedc9fac
2kuba1 <jakub.wojtyna06@gmail.com>
2023-03-06 21:51:13 +0100

    add application page

--- client/src/Pages/Application/Application.tsx
+++ client/src/Pages/Application/Application.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import useRelog from '../../Hooks/useRelog';
 import { UserContext } from '../../Contexts/UserContext';
+import { z } from 'zod';
 
 const Application = () => {
   useCloseMenu();
@@ -18,27 +19,59 @@ const Application = () => {
   const date = useRef<HTMLInputElement>(null);
   const email = useRef<HTMLInputElement>(null);
 
+  const [namesError, setNamesError] = useState(false);
+  const [ageError, setAgeError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
   const [error, setError] = useState(false);
   //todo validations
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      await axios.post(
-        'http://localhost:5259/api/application/createApplication',
-        {
-          FirstAndLastName: names.current?.value,
-          Age: age.current?.value,
-          DateOfBirth: date.current?.value,
-          Email: email.current?.value,
-        },
-        {
-          headers: {
-            Authorization: `bearer ${localStorage.getItem('token')}`,
+    setNamesError(false);
+    setAgeError(false);
+    setEmailError(false);
+
+    const namesSchama = await z
+      .string()
+      .min(3)
+      .max(60)
+      .refine(data => data.includes(' '))
+      .safeParseAsync(names.current?.value);
+
+    const ageSchema = await z
+      .number()
+      .gte(16)
+      .lte(100)
+      .safeParseAsync(Number(age.current?.value));
+
+    const emailSchema = await z
+      .string()
+      .email()
+      .safeParseAsync(email.current?.value);
+
+    if (!namesSchama.success) setNamesError(true);
+    if (!ageSchema.success) setAgeError(true);
+    if (!emailSchema.success) setEmailError(true);
+
+    if (namesSchama.success && ageSchema.success && emailSchema.success) {
+      try {
+        await axios.post(
+          'http://localhost:5259/api/application/createApplication',
+          {
+            FirstAndLastName: names.current?.value,
+            Age: age.current?.value,
+            DateOfBirth: date.current?.value,
+            Email: email.current?.value,
           },
-        }
-      );
-    } catch (error) {
-      setError(true);
+          {
+            headers: {
+              Authorization: `bearer ${localStorage.getItem('token')}`,
+            },
+          }
+        );
+      } catch (error) {
+        setError(true);
+      }
     }
   };
 
@@ -50,16 +83,28 @@ const Application = () => {
           <form onSubmit={handleSubmit}>
             <label>First and last name</label>
             <input
+              className={namesError ? styles.InputError : ''}
               ref={names}
               type='text'
               placeholder={`${User.FirstName} ${User.LastName}`}
             />
+
             <label>Age</label>
-            <input ref={age} type='number' placeholder='Age' />
+            <input
+              ref={age}
+              type='number'
+              placeholder='Age'
+              className={ageError ? styles.InputError : ''}
+            />
             <label>Date of Birth</label>
             <input ref={date} type='date' />
             <label>Contact Email</label>
-            <input ref={email} type='email' placeholder={User.Email} />
+            <input
+              ref={email}
+              type='email'
+              placeholder={User.Email}
+              className={emailError ? styles.InputError : ''}
+            />
             <input type='submit' value='Send' />
           </form>
           {error && <p className={styles.Error}>You have already applied</p>}
