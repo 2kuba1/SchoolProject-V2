19c14522987fa5411609377dc2a628560b92aa10
2kuba1 <jakub.wojtyna06@gmail.com>
2023-03-09 19:01:06 +0100

    Pre-style for admin panel

--- client/src/Pages/AdminPanel/AdminPanel.tsx
+++ client/src/Pages/AdminPanel/AdminPanel.tsx
@@ -1,8 +1,26 @@
+import styles from './AdminPanel.module.css';
 import useCloseMenu from '../../Hooks/useCloseMenu';
+import ExpandMoreImg from '../../assets/Expand_more.svg';
 
 const AdminPanel = () => {
   useCloseMenu();
-  return <div></div>;
+  return (
+    <div className={styles.AdminPaenl}>
+      <h1>Admin Panel</h1>
+      <div className={styles.Action}>
+        <p>Create Announecement</p>
+        <img src={ExpandMoreImg} alt='expand more' />
+      </div>
+      <div className={styles.Action}>
+        <p>Delete Announcement</p>
+        <img src={ExpandMoreImg} alt='expand more' />
+      </div>
+      <div className={styles.Action}>
+        <p>Check Applications</p>
+        <img src={ExpandMoreImg} alt='expand more' />
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanel;
