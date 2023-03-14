695fb52b4d0d0e116eca76219194e5fe673e7dcf
2kuba1 <jakub.wojtyna06@gmail.com>
2023-03-14 23:43:24 +0100

    Add announcement delete action

--- client/src/Pages/AdminPanel/AdminPanelActions/DeleteAnnouncementAction/DeleteAnnouncement.tsx
+++ client/src/Pages/AdminPanel/AdminPanelActions/DeleteAnnouncementAction/DeleteAnnouncement.tsx
@@ -2,6 +2,8 @@ import useAxios from '../../../../Hooks/useAxios';
 import styles from './DeleteAnnouncement.module.css';
 import { MoonLoader } from 'react-spinners';
 import GetAllAnnouncements from '../../../../Types/GetAllAnnouncements';
+import DeleteButtonImg from '../../../../assets/DleetButton.png';
+import axios from 'axios';
 
 const DeleteAnnounecement = () => {
   const [data, isPending, error, request] = useAxios<GetAllAnnouncements[]>({
@@ -36,11 +38,35 @@ const GetAnnouncements = ({
   creationDate,
   thumbnailUrl,
 }: GetAllAnnouncements) => {
+  const handleDelete = async (id: number) => {
+    try {
+      await axios.delete(`http://localhost:5259/deleteAnnouncement/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      window.location.reload();
+    }
+  };
+
   return (
     <div className={styles.GetAllAnnouncements}>
       <div className={styles.Announcement}>
         <img src={thumbnailUrl} alt='thumbnail' />
         <p>{title}</p>
+        <div className={styles.DeleteButtonParent}>
+          <button
+            className={styles.DeleteButton}
+            onClick={() => {
+              handleDelete(id);
+            }}
+          >
+            <img src={DeleteButtonImg} />
+          </button>
+        </div>
       </div>
     </div>
   );
