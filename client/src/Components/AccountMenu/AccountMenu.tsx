3c4df41d67d2d1592478c87693af180d0de33812
2kuba1 <jakub.wojtyna06@gmail.com>
2023-04-23 14:59:29 +0200

    Small changes in responsiveness

--- client/src/Components/AccountMenu/AccountMenu.tsx
+++ client/src/Components/AccountMenu/AccountMenu.tsx
@@ -6,6 +6,7 @@ import useRelog from '../../Hooks/useRelog';
 import { IsLoggedContext } from '../../Context/IsLoggedContext';
 import { UserContext } from '../../Context/UserContext';
 import useCloseMenu from '../../Hooks/useCloseMenu';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const AccountMenu = () => {
   useRelog();
@@ -30,49 +31,57 @@ const AccountMenu = () => {
   };
 
   return (
-    <div className={styles.Menu}>
-      <div className={styles.Links}>
-        {IsLogged.isLogged ? (
-          <>
-            <Link to='accountDetails'>
-              <img
-                className={styles.AccountImg}
-                src={AccountImage}
-                alt='Account icon'
-              />
-              <p>Account</p>
-            </Link>
-            <div className={styles.Logout} onClick={handleLogout}>
-              <img
-                className={styles.AccountImg}
-                src={AccountImage}
-                alt='Account icon'
-              />
-              <p>Logout</p>
-            </div>
-          </>
-        ) : (
-          <>
-            <Link to='register'>
-              <img
-                className={styles.AccountImg}
-                src={AccountImage}
-                alt='Account icon'
-              />
-              <p>Register</p>
-            </Link>
-            <Link to='login'>
-              <img
-                className={styles.AccountImg}
-                src={AccountImage}
-                alt='Account icon'
-              />
-              <p>Login</p>
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, type: 'spring' }}
+        className={styles.Menu}
+      >
+        <div className={styles.Links}>
+          {IsLogged.isLogged ? (
+            <>
+              <Link to='accountDetails'>
+                <img
+                  className={styles.AccountImg}
+                  src={AccountImage}
+                  alt='Account icon'
+                />
+                <p>Account</p>
+              </Link>
+              <div className={styles.Logout} onClick={handleLogout}>
+                <img
+                  className={styles.AccountImg}
+                  src={AccountImage}
+                  alt='Account icon'
+                />
+                <p>Logout</p>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to='register'>
+                <img
+                  className={styles.AccountImg}
+                  src={AccountImage}
+                  alt='Account icon'
+                />
+                <p>Register</p>
+              </Link>
+              <Link to='login'>
+                <img
+                  className={styles.AccountImg}
+                  src={AccountImage}
+                  alt='Account icon'
+                />
+                <p>Login</p>
+              </Link>
+            </>
+          )}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
